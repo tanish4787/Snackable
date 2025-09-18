fc1589748fd64e8bb705dafc3f10b5d834048764
Tanish Kaloiya <tanish4787@gmail.com>
2025-09-19 03:34:59 +0530

    CORS

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,10 +11,10 @@ app.use(
     credentials: true,
   })
 );
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/food", foodRoutes);
